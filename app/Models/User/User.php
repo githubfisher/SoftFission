e79690b24f8030f170a66f96084ff6534b17fc0a
githubfisher <gooodfisher@126.com>
2019-10-23 12:12:27 +0800

    chore:模型搬迁

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -1,12 +1,11 @@
 <?php
-
-namespace App;
+namespace App\Models\User;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
