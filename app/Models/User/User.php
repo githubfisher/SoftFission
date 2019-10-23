204490b34e7362d4a47e052434f5cd8e688611a9
githubfisher <gooodfisher@126.com>
2019-10-23 14:07:56 +0800

    chore:user模型添加可编辑字段

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -17,7 +17,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'mobile', 'username', 'email', 'openid', 'nickname', 'headimgurl',
     ];
 
     /**
