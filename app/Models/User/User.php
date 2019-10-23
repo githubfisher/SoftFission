d177142e25087a81dbbf2306837aade5cb5f400e
githubfisher <gooodfisher@126.com>
2019-10-23 15:01:59 +0800

    chore:定义模型对应的表名

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -7,10 +7,43 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
+/**
+ * App\Models\User\User
+ *
+ * @property int $id
+ * @property int $pid
+ * @property string $username
+ * @property string $mobile
+ * @property string $email
+ * @property string $password
+ * @property string $openid
+ * @property string $nickname
+ * @property string $headimgurl
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereHeadimgurl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereNickname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereOpenid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User wherePid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User\User whereUsername($value)
+ * @mixin \Eloquent
+ */
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
 
+    protected $table = 'user';
     /**
      * The attributes that are mass assignable.
      *
