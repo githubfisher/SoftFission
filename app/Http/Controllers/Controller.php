7048211a97ccbe3cdf54a1f277234f53dd6d479a
githubfisher <gooodfisher@126.com>
2019-10-24 10:59:03 +0800

    chore:使用dingo的HelperTrait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use Dingo\Api\Routing\Helpers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    use Helpers;
 }
