c532a26d6c1db388a2b589acfd178add31735f41
githubfisher <gooodfisher@126.com>
2019-10-24 12:07:22 +0800

    chore:添加admin及ops模型

--- app/Models/Ops/Ops.php
+++ app/Models/Ops/Ops.php
@@ -1,1 +1,32 @@
 <?php
+namespace App\Models\Ops;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class Ops extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    protected $table = 'ops';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mobile', 'username', 'email', 'openid', 'nickname', 'headimgurl',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+}
