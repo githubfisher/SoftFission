cb0b514837c1f5ece44e78f71a56f7b449878871
githubfisher <gooodfisher@126.com>
2019-10-24 23:02:51 +0800

    chore:实现多种用户的认证控制器

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -2,47 +2,61 @@
 namespace App\Http\Controllers\Api\User;
 
 use Auth;
-use http\Client\Curl\User;
+use App\Models\User\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
-class Authentication extends Controller
+class AuthController extends Controller
 {
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function register(Request $request)
     {
         $this->validate($request, [
             'mobile'           => 'mobile',
             'password'         => 'required|min:6|max:20',
-            'confirm_password' => 'required|min:6|max:20',
         ]);
 
-        $user = [
+        $user = User::create([
             'mobile'   => $request->get('mobile'),
             'email'    => $request->get('email'),
             'username' => $request->get('username'),
-            'password' => bcrypt($request->get('password')),
-        ];
-        $user  = User::create($user);
-        $token = Auth::fromUser($user);
+            'password' => Hash::make($request->get('password')),
+        ]);
+        $token = Auth::guard('user')->login($user);
 
         return response()->json(compact('token'));
     }
 
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function login(Request $request)
     {
         $this->validate($request, [
-            'mobile'   => 'required|exists:users',
+            'mobile'   => 'required|mobile|exists:user',
             'password' => 'required|string|min:6|max:20',
         ]);
 
         $credentials = $request->only(['mobile', 'password']);
 
-        return (($token = Auth::guard('api')->attempt($credentials))
-            ? response()->json(['token' => 'bearer ' . $token], 201)
+        return (($token = Auth::attempt($credentials))
+            ? response()->json(['token' => $token], 201)
             : response()->json(['error' => '账号或密码错误'], 401));
     }
 
-    public function info()
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
     {
         return response()->json(Auth::user());
     }
