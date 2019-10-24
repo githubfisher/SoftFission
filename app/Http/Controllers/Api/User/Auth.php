cffe0fa5aabe8f728da1832bd57a69692c0d0b9f
githubfisher <gooodfisher@126.com>
2019-10-24 10:59:19 +0800

    chore:添加测试controller

--- app/Http/Controllers/Api/User/Auth.php
+++ app/Http/Controllers/Api/User/Auth.php
@@ -1,1 +1,12 @@
 <?php
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+
+class Auth extends Controller
+{
+    public function index()
+    {
+        $this->response->errorBadRequest();
+    }
+}
